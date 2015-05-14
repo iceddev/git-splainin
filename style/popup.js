87379d2f6a46a90e30877eb1b5c25425ff1104e1
alysonz <alysonz@gmail.com>
2015-05-14 12:08:58 -0700

    convert popup to react

---
+++ style/popup.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var style = {
+  'selectable': {
+    'cursor': 'pointer',
+    'white-space': 'nowrap'
+  },
+  'hover': {
+    'background-color': '#D3D3D3'
+  }
+};
+
+module.exports = style;
