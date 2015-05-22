c9f3024ec90b53d5fae2017a45378875f2def52f
Brett Warner <bkwarner22@gmail.com>
2015-05-22 14:06:11 -0700

    updates how chromeBack is used

--- src/components/template-tab.js
+++ src/components/template-tab.js
@@ -5,7 +5,7 @@ const React = require('react');
 
 const client = require('../lib/client');
 const Button = require('../primed/button');
-const chromeApi = require('chromeback');
+const chromeApi = require('chromeback')(chrome);
 const getErrorMessage = require('../lib/getErrorMessage');
 
 class TemplateTab extends React.Component {
