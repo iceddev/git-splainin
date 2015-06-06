2142aaeadc776336aa980afa2d3ad6b7250d93ce
alysonz <alysonz@gmail.com>
2015-06-05 17:35:42 -0700

    give autofill it's own method

--- src/background.js
+++ src/background.js
@@ -25,11 +25,11 @@ chrome.pageAction.onClicked.addListener(function(){
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
   const regexUrl = /https:\/\/github\.com\/.*\/.*\/compare\/..*/;
-  if(regexUrl.test(tab.url) && changeInfo.url){
+  if(regexUrl.test(tab.url)){
     chrome.pageAction.show(tabId);
     chrome.storage.sync.get('autoFill', function(res){
       if(res.autoFill){
-        chrome.tabs.sendMessage(tabId, { fillPR: true });
+        chrome.tabs.sendMessage(tabId, { autoFill: true });
       }
     });
   } else {
--- src/fill-pull-body.js
+++ src/fill-pull-body.js
@@ -1,27 +1,27 @@
 'use strict';
 
 chrome.runtime.onMessage.addListener(function(request){
-  function fill(){
-    const prBodyElement = document.getElementById('pull_request_body');
-    if(prBodyElement){
-      chrome.storage.sync.get('prTemplate', function({ prTemplate }){
-        if(request.fillPR){
-          if(!prBodyElement.value.includes(prTemplate)){
-            if(prBodyElement.value){
-              prBodyElement.value += '\n';
-            }
-            prBodyElement.value = prBodyElement.value + prTemplate;
-          } else {
-            prBodyElement.value = prBodyElement.value.replace(prTemplate, '');
+  const prBodyElement = document.getElementById('pull_request_body');
+  if(prBodyElement){
+    chrome.storage.sync.get('prTemplate', function({ prTemplate }){
+      if(request.fillPR){
+        if(!prBodyElement.value.includes(prTemplate)){
+          if(prBodyElement.value){
+            prBodyElement.value += '\n';
           }
+          prBodyElement.value = prBodyElement.value + prTemplate;
+        } else {
+          prBodyElement.value = prBodyElement.value.replace(prTemplate, '');
         }
-        if(request.replaceTemplate){
-          prBodyElement.value = prBodyElement.value.replace(request.replaceTemplate, prTemplate);
+      }
+      if(request.autoFill){
+        if(!prBodyElement.value){
+          prBodyElement.value = prTemplate;
         }
-      });
-    } else {
-      setTimeout(fill, 500);
-    }
+      }
+      if(request.replaceTemplate){
+        prBodyElement.value = prBodyElement.value.replace(request.replaceTemplate, prTemplate);
+      }
+    });
   }
-  fill();
 });
